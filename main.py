aaacee7bfb28693849c67126eaee1a3750e7ea36
toumakami <apurvasarkar.official@gmail.com>
2023-06-17 23:56:43 +0530

    #

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ async def on_message(message):
         author_mention = message.author.mention
         await message.channel.send(f"Hey {author_mention}!")
 
+    #role_checker.py
     await check_message_for_role(client, message)
     return  # Stop further processing of the message
 
